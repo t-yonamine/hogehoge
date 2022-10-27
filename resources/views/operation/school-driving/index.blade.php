d2f682e45e46a4aebc769e2345708ce25048a011
phuocnth2 <phuocnth2@vnext.com.vn>
2022-10-27 17:52:54 +0700

    Task #3330: Fix comment from customer/20221026

--- resources/views/operation/school-driving/index.blade.php
+++ resources/views/operation/school-driving/index.blade.php
@@ -94,37 +94,6 @@
                 </div>
             </div>
         </div>
-        <div>
-
-            <!-- Modal -->
-            <div class="modal fade" id="modelDelete" tabindex="-1" role="dialog" aria-labelledby="modelDeleteTitle"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">消去</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            削除しますか？
-                        </div>
-                        <div class="modal-footer">
-                            <form id='formSub' action="" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
-                                <button type="submit" class="btn btn-primary">はい</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
     </div>
+    <x-modal.delete />
 @stop
-@section('js')
-    <script src="{{ asset('/js/tem-script.js') }}" type="text/javascript"></script>
-@endsection
