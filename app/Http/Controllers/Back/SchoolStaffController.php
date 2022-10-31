f9a19dcce3a18cc8c8c7d9ee782db4e466819b7a
Pham Duy Thanh <thanhpd@vnext.vn>
2022-10-31 17:44:51 +0700

    feature/thanhpd/messages

--- app/Http/Controllers/Back/SchoolStaffController.php
+++ app/Http/Controllers/Back/SchoolStaffController.php
@@ -30,7 +30,7 @@ class SchoolStaffController extends Controller
                 'school_staff_no' => 'nullable|regex:/^[a-zA-Z0-9]+$/'
             ],
             [
-                'school_staff_no' => '職員番号に正しい形式を指定してください。'
+                'school_staff_no' => Lang::get('messages.MSE00002')
             ]
         );
 
